b764f96229484af4ce42f9ad78428de7dc4dfd78
55011212150Nut <imac>
2014-10-17 15:54:20 +0700

    navigation

---
+++ navigation/navigation/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  navigation
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var colorlabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
